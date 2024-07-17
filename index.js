dae6029c581eaa2130d3e0cd4c1c5d4e3d4ebba2
404-Definitely-Found <21bce041@nirmauni.ac.in>
2024-07-18 02:59:52 +0530

    added button

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-// const text = `hi 
-// i'm
-// Chinmay`;
-// const randomEmojis = ['😀', '😁', '😂', '🤣', '😃', '😄', '😅', '😆', '😉', '😊', '😋', '😎', '😍', '😘', '🥰', '😜', '🤪', '🤩', '🥳', '😏', '😮', '😯', '😲', '😳', '🥺', '😢', '😭', '😤', '😠', '😡', '🤬', '🤯', '😱', '😨', '😰', '😥', '😓', '🤔', '🤭', '🤫', '🤥', '😶', '😐', '😑', '🙄', '😬', '😧', '😦', '😩', '😪', '😴', '🤧', '😷', '🤒', '🤕', '🤑', '🤠', '😇', '🥳', '🥸', '🥺', '🤡', '🤖', '👽', '👾', '👻', '💀', '👹', '👺', '👿', '😈', '🧛‍♂️', '🧟‍♂️', '🧞‍♂️', '🧞‍♀️', '🧙‍♂️', '🧙‍♀️', '🧚‍♂️', '🧚‍♀️', '🧜‍♂️', '🧜‍♀️', '🧝‍♂️', '🧝‍♀️'];
+const text = `hi 
+i'm
+Chinmay`;
+const randomEmojis = ['😀', '😁', '😂', '🤣', '😃', '😄', '😅', '😆', '😉', '😊', '😋', '😎', '😍', '😘', '🥰', '😜', '🤪', '🤩', '🥳', '😏', '😮', '😯', '😲', '😳', '🥺', '😢', '😭', '😤', '😠', '😡', '🤬', '🤯', '😱', '😨', '😰', '😥', '😓', '🤔', '🤭', '🤫', '🤥', '😶', '😐', '😑', '🙄', '😬', '😧', '😦', '😩', '😪', '😴', '🤧', '😷', '🤒', '🤕', '🤑', '🤠', '😇', '🥳', '🥸', '🥺', '🤡', '🤖', '👽', '👾', '👻', '💀', '👹', '👺', '👿', '😈', '🧛‍♂️', '🧟‍♂️', '🧞‍♂️', '🧞‍♀️', '🧙‍♂️', '🧙‍♀️', '🧚‍♂️', '🧚‍♀️', '🧜‍♂️', '🧜‍♀️', '🧝‍♂️', '🧝‍♀️'];
 
-// let index = 0;
-// const speed = 100; // Adjust the speed of typing here
-// const typewriterElement = document.getElementById('typewriter');
+let index = 0;
+const speed = 125; // Adjust the speed of typing here
+const typewriterElement = document.getElementById('typewriter');
+const enterButton = document.getElementById('enterButton');
 
-// function getRandomEmoji() {
-//     return randomEmojis[Math.floor(Math.random() * randomEmojis.length)];
-// }
+function getRandomEmoji() {
+    return randomEmojis[Math.floor(Math.random() * randomEmojis.length)];
+}
 
-// function type() {
-//     if (index < text.length) {
-//         typewriterElement.innerHTML = text.substring(0, index + 1) + '<span class="random-char">' + getRandomEmoji() + '</span>';
-//         index++;
-//         setTimeout(type, speed);
-//     } else {
-//         typewriterElement.innerHTML = text + '.';
-//     }
-// }
+function type() {
+    if (index < text.length) {
+        typewriterElement.innerHTML = text.substring(0, index + 1) + '<span class="random-char">' + getRandomEmoji() + '</span>';
+        index++;
+        setTimeout(type, speed);
+    } else {
+        typewriterElement.innerHTML = text + '.';
+        enterButton.classList.remove('hidden');
+        setTimeout(() => {
+            enterButton.style.opacity = 1; // Fade-in effect
+        }, 10); // Small delay to ensure the class change is rendered
+    }
+}
 
-// type();
+type();
