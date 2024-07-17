c9c09345f404c212727d8e6aecfe90478702ed5c
404-Definitely-Found <21bce041@nirmauni.ac.in>
2024-07-18 00:09:18 +0530

    Added typewriter effect

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-function myFunc() {
-    alert("You Clicked the button");
-}
+// const text = `hi 
+// i'm
+// Chinmay`;
+// const randomEmojis = ['😀', '😁', '😂', '🤣', '😃', '😄', '😅', '😆', '😉', '😊', '😋', '😎', '😍', '😘', '🥰', '😜', '🤪', '🤩', '🥳', '😏', '😮', '😯', '😲', '😳', '🥺', '😢', '😭', '😤', '😠', '😡', '🤬', '🤯', '😱', '😨', '😰', '😥', '😓', '🤔', '🤭', '🤫', '🤥', '😶', '😐', '😑', '🙄', '😬', '😧', '😦', '😩', '😪', '😴', '🤧', '😷', '🤒', '🤕', '🤑', '🤠', '😇', '🥳', '🥸', '🥺', '🤡', '🤖', '👽', '👾', '👻', '💀', '👹', '👺', '👿', '😈', '🧛‍♂️', '🧟‍♂️', '🧞‍♂️', '🧞‍♀️', '🧙‍♂️', '🧙‍♀️', '🧚‍♂️', '🧚‍♀️', '🧜‍♂️', '🧜‍♀️', '🧝‍♂️', '🧝‍♀️'];
+
+// let index = 0;
+// const speed = 100; // Adjust the speed of typing here
+// const typewriterElement = document.getElementById('typewriter');
+
+// function getRandomEmoji() {
+//     return randomEmojis[Math.floor(Math.random() * randomEmojis.length)];
+// }
+
+// function type() {
+//     if (index < text.length) {
+//         typewriterElement.innerHTML = text.substring(0, index + 1) + '<span class="random-char">' + getRandomEmoji() + '</span>';
+//         index++;
+//         setTimeout(type, speed);
+//     } else {
+//         typewriterElement.innerHTML = text + '.';
+//     }
+// }
+
+// type();
